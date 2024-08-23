21a07b5e7b5b0ceaf90c5c3b31ba09ee56d5e94f
LionaMayrona <lia.nunez.r@gmail.com>
2024-08-23 18:14:01 +0000

    ok

---
+++ Cadenas/ComparacionCadenas.java
@@ -0,0 +1,13 @@
+public class ComparacionCadenas {
+    public static void main(String[] args) {
+        // Comparacion de Cadenas (pool de cadenas)
+        var cadena1 = "Java";
+        var cadena2 = "Java";
+        // Comparacion de cadenas (==) comparan la referencia en memoria
+        System.out.print("cadena1 es igual en referencia a cadena2: ");
+        System.out.println(cadena1 == cadena2);
+
+    }
+
+    
+}
--- Cadenas/InmutabilidadCadenas.java
+++ Cadenas/InmutabilidadCadenas.java
@@ -1,11 +1,13 @@
 public class InmutabilidadCadenas {
 
     public static void main(String[] args) {
-        
-        System.out.println("oooo");
+        //Inmutabilidad de Cadenas
+        var cadena1 = "Hola";
+        System.out.println("cadena1 = " + cadena1);
+        var cadena2 = cadena1;
+        cadena1 = "Adios";
+        System.out.println("cadena1 modificada = " + cadena1);
+        System.out.println("cadena2 = " + cadena2);
 
     }
-
-
-    
 }
