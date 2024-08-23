abce097877d72d4e23b7b287a38c6da6fba99a13
LionaMayrona <lia.nunez.r@gmail.com>
2024-08-23 18:04:01 +0000

    crear clases

--- Cadenas/IndiceCadenas.java
+++ Cadenas/IndiceCadenas.java
@@ -1,7 +1,22 @@
 
+
+
 public class IndiceCadenas {
     public static void main(String[] args) {
-    System.out.println("Lia");
+        // Manejo de indices en una cadena
+        var cadena1 = "Hola Mundo";
+
+        // Recuperar el primer caracter
+        var primerCaracter = cadena1.charAt(0);
+        System.out.println("primerCaracter:" + " " + primerCaracter);
+        // Recuperar el ultimo caracter (en la posicion 9)
+        var ultimoCaracter = cadena1.charAt(9);
+        System.out.println("ultimoCaracter:" + " " + ultimoCaracter);
+
+        // Recuperar la letra M de la cadena
+        var letraM = cadena1.charAt(5);
+        System.out.println("letraM: " + letraM);
+
     }
 
 }
---
+++ Cadenas/InmutabilidadCadenas.java
@@ -0,0 +1,11 @@
+public class InmutabilidadCadenas {
+
+    public static void main(String[] args) {
+        
+        System.out.println("oooo");
+
+    }
+
+
+    
+}
