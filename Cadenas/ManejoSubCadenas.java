3b267b2a749390c9e502c10e16216664ba85b883
LionaMayrona <lia.nunez.r@gmail.com>
2024-08-23 18:31:29 +0000

    ok

---
+++ Cadenas/ManejoSubCadenas.java
@@ -0,0 +1,20 @@
+public class ManejoSubCadenas {
+    public static void main(String[] args) {
+        
+        // Tema de subcadenas
+        var cadena1 = "Hola Mundo";
+        System.out.println("cadena1 = " + cadena1);
+
+        // Subcadena
+        var subcadena1 = cadena1.substring(0, 4);
+        System.out.println("subcadena1 = " + subcadena1);
+
+        // Subcadena de Mundo
+        var subcadena2 = cadena1.substring(5);
+        System.out.println("subcadena2 = " + subcadena2);
+
+
+    }
+
+
+}
